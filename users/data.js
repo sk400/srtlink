65fab54f023b07e798f2cf4d4a79063d5ce85e30
[sk developer] <[skdeveloper101@gmail.com]>
2024-05-11 18:20:26 +0530

    Add data.js file

---
+++ users/data.js
@@ -0,0 +1,7 @@
+const user = {
+  name: "Shubham Sarda",
+  username: "shubhamsarda",
+  avatar: "https://i.pravatar.cc/300?img=1",
+  bio: "Hey Shubham This Side!",
+  link: "https://github.com/ShubhamSarda",
+};
